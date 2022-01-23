dc6aadcae54cda647e81f11e44a170fe9cd04464
sk-lakkakula <sk.lakkakula@gmail.com>
2022-01-23 11:52:33 +0530

    CONFIG SERVER MICROSERVICE TO SERVE THE DETAILS OF PROPERTIES BY ENVIRONMENT

---
+++ src/test/java/com/lsk/apps/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsk.apps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
